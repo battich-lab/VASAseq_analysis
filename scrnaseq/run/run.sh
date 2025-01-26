84cc6c40751c2aa759c39fe710de2e4f15ccd299
Nico Trummer <nictru32@gmail.com>
2025-01-26 17:11:53 +0100

    Perform first kallisto run

--- scrnaseq/run/run.sh
+++ scrnaseq/run/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nextflow run ../pipeline -profile daisybio,keep_work,apptainer,mouse,star_plate -resume
+nextflow run ../pipeline -profile daisybio,keep_work,apptainer,mouse,kallisto_plate -resume
